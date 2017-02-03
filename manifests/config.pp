05497f4dd184c58373f8d596b8f8f4d3165d9d8c
Jared Nelson <jared@triplevia.com>
2017-02-03 14:14:47 -0500

    use *.local config

--- manifests/config.pp
+++ manifests/config.pp
@@ -5,52 +5,52 @@ class postgrey::config() {
   }
 
   concat {
-    '/etc/postgrey/whitelist_clients':
+    '/etc/postgrey/whitelist_clients.local':
   }
 
-  concat::fragment { 'postgrey/whitelist_clients/header':
-     target => '/etc/postgrey/whitelist_clients',
+  concat::fragment { 'postgrey/whitelist_clients.local/header':
+     target => '/etc/postgrey/whitelist_clients.local',
      order  => 00,
      source => 'puppet:///modules/postgrey/whitelist_clients.header';
   }
 
   if ($postgrey::default_whitelist_clients == true) {
-    concat::fragment { 'postgrey/whitelist_clients/default':
-      target => '/etc/postgrey/whitelist_clients',
+    concat::fragment { 'postgrey/whitelist_clients.local/default':
+      target => '/etc/postgrey/whitelist_clients.local',
       order  => 10,
       source => 'puppet:///modules/postgrey/whitelist_clients.default';
     }
   }
 
   if ($postgrey::whitelist_clients != []) {
-    concat::fragment { 'postgrey/whitelist_clients':
-      target  => '/etc/postgrey/whitelist_clients',
+    concat::fragment { 'postgrey/whitelist_clients.local':
+      target  => '/etc/postgrey/whitelist_clients.local',
       order   => 15,
-      content => inline_template('<%= scope.lookupvar("postgrey::whitelist_clients").join("\n") %>')
+      content => inline_template('<%= scope.lookupvar("postgrey::whitelist_clients").join("\n") %><%= "\n" %>')
     }
   }
 
   concat {
-    '/etc/postgrey/whitelist_recipients':
+    '/etc/postgrey/whitelist_recipients.local':
   }
 
-  concat::fragment { 'postgrey/whitelist_recipients/header':
-     target => '/etc/postgrey/whitelist_recipients',
+  concat::fragment { 'postgrey/whitelist_recipients.local/header':
+     target => '/etc/postgrey/whitelist_recipients.local',
      order  => 00,
      source => 'puppet:///modules/postgrey/whitelist_recipients.header';
   }
 
   if ($postgrey::default_whitelist_recipients == true) {
-    concat::fragment { 'postgrey/whitelist_recipients/default':
-      target => '/etc/postgrey/whitelist_recipients',
+    concat::fragment { 'postgrey/whitelist_recipients.local/default':
+      target => '/etc/postgrey/whitelist_recipients.local',
       order  => 10,
       source => 'puppet:///modules/postgrey/whitelist_recipients.default';
     }
   }
 
   if ($postgrey::whitelist_recipients != []) {
-    concat::fragment { 'postgrey/whitelist_recipients':
-      target  => '/etc/postgrey/whitelist_recipients',
+    concat::fragment { 'postgrey/whitelist_recipients.local':
+      target  => '/etc/postgrey/whitelist_recipients.local',
       order   => 15,
       content => inline_template('<%= scope.lookupvar("postgrey::whitelist_recipients").join("\n") %>')
     }
