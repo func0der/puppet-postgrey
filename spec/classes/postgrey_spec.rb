6941209039488b6931def834af5075fb8371737d
func0der <529819+func0der@users.noreply.github.com>
2020-04-05 22:10:46 +0200

    Add fact overwrite.

--- spec/classes/postgrey_spec.rb
+++ spec/classes/postgrey_spec.rb
@@ -3,6 +3,12 @@
 require 'spec_helper'
 
 describe 'postgrey' do
+  let(:facts) { {
+      :os => {
+          :family => 'Debian',
+      },
+  } }
+
   it { should include_class('postgrey::config') }
   it { should include_class('postgrey::service') }
   it { should include_class('postgrey::install') }
